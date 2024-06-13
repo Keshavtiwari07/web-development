37645dd94c2464a0f760aafb898b3ea617e09d96
keshav tiwari <138362706+Keshavtiwari07@users.noreply.github.com>
2024-06-14 00:31:44 +0530

    .

---
+++ apply-transform-over-each-element-in-array.js
@@ -0,0 +1,10 @@
+
+var map = function(arr, fn) {
+    const transformedArr = [];
+    let index = 0;
+    for (const element of arr) {
+      transformedArr[index] = fn(element, index);
+      index++;
+    }
+    return transformedArr;
+  };
